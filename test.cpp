c0b928fd4909555ff343440531c50cd9414ddff6
Eric May <yaromesky@gmail.com>
2022-05-02 14:57:22 -0700

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+  Object o;
+  std::cout << "tests being run...\n";
+  if(o.Cubed(-3) == -27)
+  {
+    std::cout << "First test passed\n";
+  }
+  else std::cout << "First test failed\n";
+  return 0;
+}
